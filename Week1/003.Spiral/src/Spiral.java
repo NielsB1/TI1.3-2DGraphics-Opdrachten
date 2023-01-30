92a9d60ca37c0384b694ad076ef4dd136ab3438f
NielsB1 <n.buijs@student.avans.nl>
2023-01-30 15:18:15 +0000

    spiraaltje

--- Week1/003.Spiral/src/Spiral.java
+++ Week1/003.Spiral/src/Spiral.java
@@ -1,7 +1,10 @@
 import java.awt.*;
 import java.awt.geom.*;
+
 import javafx.application.Application;
+
 import static javafx.application.Application.launch;
+
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -17,13 +20,35 @@ public class Spiral extends Application {
         primaryStage.setTitle("Spiral");
         primaryStage.show();
     }
-    
-    
-    public void draw(FXGraphics2D graphics) {
+
+
+    public void draw(FXGraphics2D graphics) throws InterruptedException {
+        graphics.translate(1920 / 2, 1080 / 2);
+        graphics.scale(1, -1);
+
+        double resolution = 0.1;
+        double scale = 40.0;
+        double lastY = Math.sin(0);
+        double lastX = Math.cos(0);
+        float straal = 1;
+
+        for (int j = 0; j < 10; j++) {
+            for (double i = 0; i < 2 * Math.PI; i += resolution) {
+                float x = (float) (straal * Math.cos(i));
+                float y = (float) (straal * Math.sin(i));
+                graphics.draw(new Line2D.Double(x * scale, y * scale, lastX * scale, lastY * scale));
+                lastY = y;
+                lastX = x;
+                straal += 0.05;
+            }
+        }
+
+        graphics.setColor(Color.red);
+        graphics.drawLine(-1000, 0, 1000, 0); // x as
+        graphics.drawLine(0, 1000, 0, -1000); // y as
     }
-    
-    
-    
+
+
     public static void main(String[] args) {
         launch(Spiral.class);
     }
