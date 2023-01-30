22df08eb497951a0e9ed8d61e69334ef07a8314f
NielsB1 <n.buijs@student.avans.nl>
2023-01-30 15:43:34 +0000

    regenboog

--- Week1/004.Rainbow/src/Rainbow.java
+++ Week1/004.Rainbow/src/Rainbow.java
@@ -1,7 +1,10 @@
 import java.awt.*;
 import java.awt.geom.*;
+
 import javafx.application.Application;
+
 import static javafx.application.Application.launch;
+
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -17,14 +20,35 @@ public class Rainbow extends Application {
         primaryStage.setTitle("Rainbow");
         primaryStage.show();
     }
-    
-    
+
+
     public void draw(FXGraphics2D graphics) {
 
+        graphics.translate(1920 / 2, 1080 / 2);
+        graphics.scale(1, -1);
+
+        double resolution = 0.001;
+        float radiusBinnen = 300;
+        float radiusBuiten = 500;
+
+        for (float i = 0; i < Math.PI; i += resolution) {
+            float x1 = (float) (radiusBinnen * Math.cos(i));
+            float y1 = (float) (radiusBinnen * Math.sin(i));
+            float x2 = (float) (radiusBuiten * Math.cos(i));
+            float y2 = (float) (radiusBuiten * Math.sin(i));
+
+            graphics.setColor(Color.getHSBColor((float) (i / Math.PI), 0.7f, 1));
+            graphics.draw(new Line2D.Float(x1, y1, x2, y2));
+
+        }
+
+
+        graphics.setColor(Color.red);
+        graphics.drawLine(-1000, 0, 1000, 0); // x as
+        graphics.drawLine(0, 1000, 0, -1000); // y as
     }
-    
-    
-    
+
+
     public static void main(String[] args) {
         launch(Rainbow.class);
     }
