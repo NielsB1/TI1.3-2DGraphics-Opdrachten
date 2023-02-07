840c5f6ff0434c779b734f7b418abf1c5916f7c8
NielsB1 <n.buijs@student.avans.nl>
2023-02-07 12:06:50 +0000

    Yong Bong Ding Dong

--- Week2/002.YingYang/src/YingYang.java
+++ Week2/002.YingYang/src/YingYang.java
@@ -16,8 +16,7 @@ public class YingYang extends Application {
     private ResizableCanvas canvas;
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
@@ -28,16 +27,49 @@ public class YingYang extends Application {
     }
 
 
-    public void draw(FXGraphics2D graphics)
-    {
+    public void draw(FXGraphics2D graphics) {
+
+
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        graphics.translate(1920 / 2, 1080 / 2);
+        graphics.scale(5, -5);
+
+
+        Area circle = new Area(new Ellipse2D.Double(-50, -50, 100, 100)); //main circle
+        Area circle2 = new Area(new Ellipse2D.Double(-25, -50, 50, 50)); //bottom black circle
+        Area circle3 = new Area(new Ellipse2D.Double(-25, 0, 50, 50)); //top white circle
+        Area circle4 = new Area(new Rectangle.Double(-100,-50,100,100)); //weird rectangle to subtract circle 1
+        Area circle5 = new Area(new Ellipse2D.Double(-50, -50, 100, 100)); //copy of circle 1 top draw a outline
+
+
+        Area circle6 = new Area(new Ellipse2D.Double(-5, 20, 10, 10)); //small black circle
+        Area circle7 = new Area(new Ellipse2D.Double(-5, -30, 10, 10)); //small white circle
+
+
+
+
+        circle.subtract(circle4);
+
+        graphics.draw(circle2);
+        graphics.fill(circle2);
+        graphics.fill(circle);
+
+        graphics.setColor(Color.white);
+        graphics.fill(circle3);
+        graphics.fill(circle7);
+
+        graphics.setColor(Color.black);
+        graphics.draw(circle5);
+
+        graphics.fill(circle6);
+
     }
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(YingYang.class);
     }
 
