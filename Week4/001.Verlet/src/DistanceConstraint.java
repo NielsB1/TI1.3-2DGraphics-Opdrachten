355acde1b2a40cccb0b00ab3b774b16bd9010743
NielsB1 <n.buijs@student.avans.nl>
2023-02-27 17:12:25 +0100

    tried to make color shift dependign on the force on a constraint

--- Week4/001.Verlet/src/DistanceConstraint.java
+++ Week4/001.Verlet/src/DistanceConstraint.java
@@ -1,4 +1,6 @@
 import org.jfree.fx.FXGraphics2D;
+
+import java.awt.*;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
@@ -8,6 +10,10 @@ public class DistanceConstraint implements Constraint {
     private Particle a;
     private Particle b;
 
+    private double force;
+
+    private Color color;
+
     public DistanceConstraint(Particle a, Particle b) {
         this(a, b, a.getPosition().distance(b.getPosition()));
     }
@@ -16,13 +22,35 @@ public class DistanceConstraint implements Constraint {
         this.a = a;
         this.b = b;
         this.distance = distance;
+        this.force = 0.0;
+        this.color = Color.green;
     }
 
     @Override
     public void satisfy() {
 
         double currentDistance = a.getPosition().distance(b.getPosition());
-        double adjustmentDistance = (currentDistance - distance) / 2;
+        this.force = (currentDistance - distance) / 2;
+
+        //todo monkey code not working, please fix future me :D
+        float hue = 1 / 360f * 120f;
+
+        if (force > 0) {
+            if ((hue -= force) > 0 && (hue -= force) < 120)
+                hue = (float) (1 / 360f * (120f - force));
+        }
+        if (hue < 0.0f) {
+            hue = 0;
+        }
+
+        if (hue > 1 / 360f * 120f) {
+            hue = 1 / 360f * 120f;
+        }
+
+        System.out.println(force + " - " + hue);
+        Color color = Color.getHSBColor(hue, 1, 1);
+        this.color = color;
+
 
         Point2D BA = new Point2D.Double(b.getPosition().getX() - a.getPosition().getX(), b.getPosition().getY() - a.getPosition().getY());
         double length = BA.distance(0, 0);
@@ -33,14 +61,19 @@ public class DistanceConstraint implements Constraint {
             BA = new Point2D.Double(1, 0);
         }
 
-        a.setPosition(new Point2D.Double(a.getPosition().getX() + BA.getX() * adjustmentDistance,
-                a.getPosition().getY() + BA.getY() * adjustmentDistance));
-        b.setPosition(new Point2D.Double(b.getPosition().getX() - BA.getX() * adjustmentDistance,
-                b.getPosition().getY() - BA.getY() * adjustmentDistance));
+        a.setPosition(new Point2D.Double(a.getPosition().getX() + BA.getX() * force,
+                a.getPosition().getY() + BA.getY() * force));
+        b.setPosition(new Point2D.Double(b.getPosition().getX() - BA.getX() * force,
+                b.getPosition().getY() - BA.getY() * force));
     }
 
     @Override
     public void draw(FXGraphics2D g2d) {
+        g2d.setColor(getColor());
         g2d.draw(new Line2D.Double(a.getPosition(), b.getPosition()));
+    }
+
+    public Color getColor() {
+        return color;
     }
 }
