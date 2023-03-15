1aafc7b8bd1229eef2ce058ac9504b9c75f31c8e
NielsB1 <n.buijs@student.avans.nl>
2023-03-15 14:10:24 +0000

    jep

--- Eindopdracht/src/Eindopdracht.java
+++ Eindopdracht/src/Eindopdracht.java
@@ -34,6 +34,7 @@ public class Eindopdracht extends Application {
     private Body floor;
     private NoiseMapGenerator noiseMapGenerator;
     private ArrayList<Line2D> lines = new ArrayList<>();
+    private boolean firstDraw = true;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -83,30 +84,30 @@ public class Eindopdracht extends Application {
         world.setGravity(new Vector2(0, -50));
 
         noiseMapGenerator = new NoiseMapGenerator();
-        for (int i = 0; i < 10000; i+= 50) {
-            lines.add(new Line2D.Double(i, noiseMapGenerator.noise(i/1000f) * 1000, i + 50, noiseMapGenerator.noise((i + 50) / 1000f) * 1000));
+        for (int i = 0; i < 3000; i += 50) {
+            lines.add(new Line2D.Double(i, noiseMapGenerator.noise(i / 1000f) * 1000, i + 50, noiseMapGenerator.noise((i + 50) / 1000f) * 1000));
             Body line = new Body();
-            Point2D topLeft = new Point2D.Double(i, noiseMapGenerator.noise(i/1000f) * 1000);
+            Point2D topLeft = new Point2D.Double(i, noiseMapGenerator.noise(i / 1000f) * 1000);
             Point2D bottomRight = new Point2D.Double(i + 50, noiseMapGenerator.noise((i + 50) / 1000f) * 1000);
             Vector2 v1 = new Vector2(((Point2D.Double) topLeft).x, ((Point2D.Double) topLeft).y);
             Vector2 v2 = new Vector2(((Point2D.Double) bottomRight).x, ((Point2D.Double) bottomRight).y);
-            double y = v2.y + ((v1.y - v2.y) /2);
+            double y = v2.y + ((v1.y - v2.y) / 2);
             double x = v1.x + 25;
-            line.addFixture(Geometry.createRectangle(60, 10));
             line.getTransform().setTranslation(x, y);
-            line.rotate(Math.atan((v2.y - v1.y) /(v2.x - v1.x) ), new Vector2(x, y));
+            line.rotate(Math.atan((v2.y - v1.y) / (v2.x - v1.x)), new Vector2(x, y));
+                line.addFixture(Geometry.createRectangle(40 + (Math.abs(line.getTransform().getRotation()) * 42), 5));
             line.setMass(MassType.INFINITE);
             world.addBody(line);
         }
         car = new Body();
-        car.addFixture(Geometry.createRectangle(100,40));
+        car.addFixture(Geometry.createRectangle(100, 40));
         car.getTransform().setTranslation(-300, 100);
         car.setMass(MassType.NORMAL);
         world.addBody(car);
 
         leftWheel = new Body();
         leftWheel.addFixture(Geometry.createCircle(10));
-        leftWheel.getFixture(0).setFriction(10);
+        leftWheel.getFixture(0).setFriction(1);
         leftWheel.getTransform().setTranslation(-330, 80);
         leftWheel.setMass(MassType.NORMAL);
         world.addBody(leftWheel);
@@ -119,14 +120,14 @@ public class Eindopdracht extends Application {
         world.addBody(rightWheel);
 
         //todo make monkey springs works
-        Joint leftSpring = new PinJoint(leftWheel,car.getTransform().getTranslation(),10, 10, 10);
-        Joint rightSpring = new PinJoint(rightWheel,car.getTransform().getTranslation(),10, 10, 10);
+        Joint leftSpring = new PinJoint(leftWheel, car.getTransform().getTranslation(), 10, 1, 10);
+        Joint rightSpring = new PinJoint(rightWheel, car.getTransform().getTranslation(), 10, 1, 10);
 
 
         floor = new Body();
-        floor.addFixture(Geometry.createRectangle(2000, 10));
+        floor.addFixture(Geometry.createRectangle(2000, 5));
         floor.getFixture(0).setFriction(3);
-        floor.getTransform().setTranslation(-1000, 0);
+        floor.getTransform().setTranslation(-1000, 0.5);
         floor.setMass(MassType.INFINITE);
         world.addBody(floor);
     }
@@ -143,9 +144,9 @@ public class Eindopdracht extends Application {
 
         graphics.setColor(Color.green);
         graphics.setStroke(new BasicStroke(3));
-        for (Line2D line : lines) {
-            graphics.draw(line);
-        }
+            for (Line2D line : lines) {
+                graphics.draw(line);
+            }
 
         for (GameObject go : gameObjects) {
             go.draw(graphics);
@@ -162,6 +163,9 @@ public class Eindopdracht extends Application {
     public void update(double deltaTime) {
         mousePicker.update(world, camera.getTransform((int) canvas.getWidth(), (int) canvas.getHeight()), 1);
         world.update(deltaTime);
+        leftWheel.applyImpulse(-10000000);
+        rightWheel.applyImpulse(-10000000);
+
     }
 
     public static void main(String[] args) {
