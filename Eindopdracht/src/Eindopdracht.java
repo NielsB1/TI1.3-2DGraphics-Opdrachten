2168caf684ed264418532321af4b31c6bc9f7f58
NielsB1 <n.buijs@student.avans.nl>
2023-03-17 13:10:16 +0000

    added player control, driver head and statement for when the game is over

--- Eindopdracht/src/Eindopdracht.java
+++ Eindopdracht/src/Eindopdracht.java
@@ -1,7 +1,6 @@
 
 import java.awt.*;
 import java.awt.geom.*;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -12,15 +11,14 @@ import static javafx.application.Application.launch;
 
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import org.dyn4j.collision.CategoryFilter;
-import org.dyn4j.collision.Filter;
 import org.dyn4j.dynamics.Body;
 import org.dyn4j.dynamics.World;
 import org.dyn4j.dynamics.joint.*;
 import org.dyn4j.geometry.Geometry;
-import org.dyn4j.geometry.Mass;
 import org.dyn4j.geometry.MassType;
 import org.dyn4j.geometry.Vector2;
 import org.jfree.fx.FXGraphics2D;
@@ -46,6 +44,8 @@ public class Eindopdracht extends Application {
 
         // Add debug button
         javafx.scene.control.CheckBox showDebug = new CheckBox("Show debug");
+        showDebug.setFocusTraversable(false);
+
         showDebug.setOnAction(e -> {
             debugSelected = showDebug.isSelected();
         });
@@ -56,6 +56,10 @@ public class Eindopdracht extends Application {
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
 
         camera = new Camera(canvas, g -> draw(g), g2d);
+        canvas.setFocusTraversable(true);
+
+        canvas.setOnKeyPressed(event -> onKeyPressed(event));
+        canvas.setOnKeyReleased(event -> onKeyReleased(event));
 
         new AnimationTimer() {
             long last = -1;
@@ -78,6 +82,16 @@ public class Eindopdracht extends Application {
         draw(g2d);
     }
 
+    private KeyCode currentKey = null;
+
+    private void onKeyReleased(KeyEvent event) {
+        currentKey = null;
+    }
+
+    private void onKeyPressed(KeyEvent event) {
+        currentKey = event.getCode();
+    }
+
     private Body car;
     private Body leftWheel;
     private Body rightWheel;
@@ -85,11 +99,12 @@ public class Eindopdracht extends Application {
     private WheelJoint rightSpring;
     private Body leftSpringPoint;
     private Body rightSpringPoint;
+    private Body driverHead;
 
     private ArrayList<Body> groundBodies = new ArrayList<>();
 
-    private int resolution = 20;
-    private int amplitude = 300;
+    private int resolution = 100;
+    private int amplitude = 500;
     private double distance = 1000;
 
     public void init() {
@@ -120,7 +135,7 @@ public class Eindopdracht extends Application {
         }
         car = new Body();
         car.addFixture(Geometry.createRectangle(2.4, .30));
-        car.addFixture(Geometry.createRectangle(1, .70));
+        car.addFixture(Geometry.createRectangle(1, .65));
 
         car.getTransform().setTranslation(-3, 1);
         car.setMass(MassType.NORMAL);
@@ -143,7 +158,7 @@ public class Eindopdracht extends Application {
         leftWheel.getTransform().setTranslation(leftSpringPoint.getTransform().getTranslation());
         leftWheel.setMass(MassType.NORMAL);
         world.addBody(leftWheel);
-        gameObjects.add(new GameObject("Everything/Tire.png", leftWheel, new Vector2(leftWheel.getTransform().getTranslationX() + 2.5, leftWheel.getTransform().getTranslationY()), 0.85));
+        gameObjects.add(new GameObject("Everything/Tire.png", leftWheel, new Vector2(leftWheel.getTransform().getTranslationX() + 2.7, leftWheel.getTransform().getTranslationY() - 0.1), 0.85));
 
 
         leftSpring = new WheelJoint(leftWheel, car, leftSpringPoint.getTransform().getTranslation(), new Vector2(0, 1));
@@ -165,7 +180,7 @@ public class Eindopdracht extends Application {
         rightWheel.getTransform().setTranslation(rightSpringPoint.getTransform().getTranslation());
         rightWheel.setMass(MassType.NORMAL);
         world.addBody(rightWheel);
-        gameObjects.add(new GameObject("Everything/Tire.png", rightWheel, new Vector2(rightWheel.getTransform().getTranslationX() + 2.5, rightWheel.getTransform().getTranslationY()), 0.85));
+        gameObjects.add(new GameObject("Everything/Tire.png", rightWheel, new Vector2(rightWheel.getTransform().getTranslationX() + 2.5, rightWheel.getTransform().getTranslationY() - 0.1), 0.85));
 
         rightSpring = new WheelJoint(rightWheel, car, rightSpringPoint.getTransform().getTranslation(), new Vector2(0, 1));
         rightSpring.setDampingRatio(0.3);
@@ -175,6 +190,18 @@ public class Eindopdracht extends Application {
 
         world.addJoint(leftSpring);
         world.addJoint(rightSpring);
+
+
+        driverHead = new Body();
+        driverHead.addFixture(Geometry.createRectangle(0.4, 0.4));
+        driverHead.setMass(MassType.NORMAL);
+        driverHead.getTransform().setTranslation(car.getTransform().getTranslationX(), car.getTransform().getTranslationY() + 0.6);
+        world.addBody(driverHead);
+        gameObjects.add(new GameObject("Everything/driver-head.png", driverHead, new Vector2(driverHead.getTransform().getTranslationX(), driverHead.getTransform().getTranslationY()), 1));
+
+        RevoluteJoint headToCar = new RevoluteJoint(driverHead, car, new Vector2(car.getTransform().getTranslationX(), car.getTransform().getTranslationY() + 0.5));
+        headToCar.setCollisionAllowed(true);
+        world.addJoint(headToCar);
 
 
         floor = new Body();
@@ -214,23 +241,58 @@ public class Eindopdracht extends Application {
         }
 
         graphics.setTransform(originalTransform);
+
     }
 
     public void update(double deltaTime) {
         world.update(deltaTime);
-        leftWheel.applyImpulse(-0.008);
-        rightWheel.applyImpulse(-0.008);
-        mousePicker.update(world, camera.getTransform((int) canvas.getWidth(), (int) canvas.getHeight()), scale);
+
+        int x = (int) (driverHead.getTransform().getTranslationX() * scale);
+        if (driverHead.getTransform().getTranslationX() > 0)
+
+
+            if ((driverHead.getTransform().getTranslationY() * scale) - (noiseMapGenerator.noise(x / distance) * amplitude) < 50) {
+                //todo monkey do be dead, return to start screen
+            }
+
+//        mousePicker.update(world, camera.getTransform((int) canvas.getWidth(), (int) canvas.getHeight()), scale);
+
+        if (currentKey != null) {
+            if (currentKey.equals(KeyCode.W)) {
+                driveForwards();
+            } else if (currentKey.equals(KeyCode.S)) {
+                driveBackwards();
+            } else if (currentKey.equals(KeyCode.A)) {
+                rotateLeft();
+            } else if (currentKey.equals(KeyCode.D)) {
+                rotateRight();
+            }
+        }
 
         if (groundBodies.get((groundBodies.size() / 3) * 2).getTransform().getTranslationX() < car.getTransform().getTranslationX()) {
-//            System.out.println("ik moet meer maken" + "\tpositie: " + car.getTransform().getTranslationX());
             generateGround(100);
         } else if (groundBodies.get(groundBodies.size() / 3).getTransform().getTranslationX() > car.getTransform().getTranslationX()
                 && car.getTransform().getTranslationX() > 100) {
-
-//            System.out.println("ik moet vorige genereren" + "\tpositie: " + car.getTransform().getTranslationX());
             generatePreviousGround();
         }
+    }
+
+    private void rotateRight() {
+        car.applyImpulse(-0.02);
+    }
+
+    private void rotateLeft() {
+        car.applyImpulse(0.02);
+    }
+
+    private void driveBackwards() {
+        leftWheel.applyImpulse(0.05);
+        rightWheel.applyImpulse(0.05);
+    }
+
+    private void driveForwards() {
+        leftWheel.applyImpulse(-0.05);
+        rightWheel.applyImpulse(-0.05);
     }
 
     public void generateGround(int amount) {
