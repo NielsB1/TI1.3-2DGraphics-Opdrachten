5cd72cad50cdb8a93505ce53f87e51b301be9dbf
NielsB1 <n.buijs@student.avans.nl>
2023-03-22 18:24:31 +0000

    update loading playerstats

--- Eindopdracht/src/PlayerStatsLoaderAndSaver.java
+++ Eindopdracht/src/PlayerStatsLoaderAndSaver.java
@@ -6,7 +6,7 @@ public class PlayerStatsLoaderAndSaver {
     public void save(PlayerStats playerStats) {
         try {
             String filePath = "Eindopdracht/resources/playerStats.txt";
-            File file = new File(filePath);
+            File file = new File(String.valueOf(this.getClass().getResource("playerStats.txt")));
             FileOutputStream fileOut = new FileOutputStream(file);
             ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
             objectOut.writeObject(playerStats);
@@ -19,14 +19,16 @@ public class PlayerStatsLoaderAndSaver {
     }
 
     public PlayerStats load() throws Exception {
-            String filePath = "Eindopdracht/resources/playerStats.txt";
-            File file = new File(filePath);
-            FileInputStream fileIn = new FileInputStream(file);
-            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("playerStats.txt");
+//        String filePath = "Eindopdracht/resources/playerStats.txt";
+//            File file = new File(filePath);
+//            FileInputStream fileIn = new FileInputStream(file);
+            ObjectInputStream objectIn = new ObjectInputStream(resourceAsStream);
             PlayerStats playerStats = (PlayerStats) objectIn.readObject();
             objectIn.close();
-            fileIn.close();
-            System.out.println("PlayerStats loaded successfully from " + filePath);
+//            fileIn.close();
+            System.out.println("PlayerStats loaded successfully");
+        System.out.println(playerStats.getHighScore());
             return playerStats;
     }
 }
